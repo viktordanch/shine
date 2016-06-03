a243c762b84d9ecdf947fdedc43780885c39fa41
viktordanch <viktor.danch@gmail.com>
2016-06-03 23:35:33 +0300

    uptimize shearch

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,12 +1,17 @@
 class CustomersController < ApplicationController
+  PAGE_SIZE = 10
+
   def index
-    # @customers = Customer.all.limit(10)
+    @page = (params[:page] || 0).to_i
+
     if params[:keywords].present?
       @keywords = params[:keywords]
       customer_search_term = CustomerSearchTerm.new(@keywords)
       @customers = Customer.where(customer_search_term.where_clause,
                                   customer_search_term.where_args,
                                   customer_search_term.order)
+                            .offset(PAGE_SIZE * @page)
+                            .limit(PAGE_SIZE)
     else
       @customers = []
     end
